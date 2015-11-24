c286aa2ad78e33269cebd27b05e56e0806b19544
Speedy <speedyunicorn777@gmail.com>
2015-11-24 14:27:47 -0800

    initial commit

---
+++ MyFirstApp/ViewControllerTwo.swift
@@ -0,0 +1,26 @@
+//
+//  ViewControllerTwo.swift
+//  MyFirstApp
+//
+//  Created by Nikita Fill on 11/24/15.
+//  Copyright © 2015 Nikita Fill. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
