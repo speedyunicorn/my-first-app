5afffd3e25ca76f0465d478d7e870ad17230d1c8
Speedy <speedyunicorn777@gmail.com>
2015-11-24 22:28:34 -0800

    adding readme.md

---
+++ MyFirstApp/ViewController4.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController4.swift
+//  MyFirstApp
+//
+//  Created by Nikita Fill on 11/24/15.
+//  Copyright © 2015 Nikita Fill. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController4: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
